05c9a95663d75c6f27d1e13a0abb159816bc4b2e
Anto Christopher <anto@deepsource.io>
2023-06-07 10:22:00 +0530

    google format test

--- Format.java
+++ Format.java
@@ -1,4 +1,7 @@
 public class Format {
 public static void main(String[] args)     {
+Map.Entry<URL, ConfigData>[]
+entries = (Map.Entry<URL, ConfigData>[])
+configs.entrySet().toArray(new Map.Entry[0]);
 }
 }
